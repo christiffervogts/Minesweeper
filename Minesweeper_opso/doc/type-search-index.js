ca0db354ff9d2c0150071f08e58c01aa95d40197
Christiffer Vogts <christiffer.vogts221@gmail.com>
2026-03-13 19:46:18 -0400

    Delete Minesweeper_opso/doc directory

--- Minesweeper_opso/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"main","l":"Easy"},{"p":"main","l":"Hard"},{"p":"main","l":"Insane"},{"p":"main","l":"Main"},{"p":"main","l":"Medium"},{"p":"main","l":"Setup"}];updateSearchResults();
